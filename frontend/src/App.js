3028212c21b02e22170722cc271e2cfbb24caa6b
walterfilosa <walterfilosa03@gmail.com>
2025-11-30 19:22:03 +0100

    Prime modifiche interfaccia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './Navbar';
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
     <div className="App">
+        <Navbar></Navbar>
       <h1>Segnala issue</h1>
     </div>
   );
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function Navbar() {
+    return (
+        <nav className = "navbar">
+            <img src={'src/Logo/LogoBugBoard26-Con Scritta.png'} alt={"Logo"}>
+
+            </img>
+        </nav>
+    )
+}

15d0552fdfc1e7aa5c20235e858f3cb62b7fce9f
walterfilosa <walterfilosa03@gmail.com>
2025-12-11 14:28:04 +0100

    Modifiche interfaccia

---
+++ frontend/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'About.css';
+
+export default function About() {
+    return (
+        <div className="homepage-container">
+            <h1>
+                About
+            </h1>
+            <p>
+                Scopri chi siamo
+            </p>
+        </div>
+    )
+}
